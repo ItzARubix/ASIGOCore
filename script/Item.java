8ee6ac9f50b1cb959199306bd4adb01da1dc20b3
ItzARubix <83780010+ItzARubix@users.noreply.github.com>
2024-08-08 20:07:10 -0400

    Delete script directory

--- script/Item.java
+++
@@ -1,7 +0,0 @@
-public class Item extends Card {
-	//I imagine this to be the prime example of "pointless abstraction."
-	//This class is empty, because items are meant to be the "purest" form of cards.
-	//They only have an activated effect, which is defined for all other cards as well.
-	//"So why make the Card.java class abstract?" I hear you ask.
-	//Well... that's a very good question. I have no idea. 
-}
